a56844a931429b6f40b93e1506c451bb8f632ad6
Danny Evins <dcastkid@gmail.com>
2016-08-05 15:17:26 -0500

    working on  transitioning from "new" route

---
+++ app/todo-group/new/controller.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    addCategory(formValues) {
+      this.store.createRecord('todo-group', formValues)
+        .save().then(() => {
+          this.transitionToRoute(index);
+        });
+    },
+  },
+});
